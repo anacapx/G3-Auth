4e0e336bbbdc83da59a75772c0a31b04c0561024
lisdrl <lisandre.darioli@gmail.com>
2022-06-07 14:37:32 -0300

    feat(back): creates package and classes used for security configurations

---
+++ src/main/java/com/g3/auth/config/security/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.g3.auth.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.g3.auth.repository.AdminRepository;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationService authService;
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private AdminRepository adminRepository;
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(authService)
+		.passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/auth").permitAll()
+		.antMatchers(HttpMethod.POST, "/admin").permitAll()
+		.anyRequest().authenticated()
+		.and().csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(new TokenBasedAuthenticationFilter(tokenService, adminRepository), UsernamePasswordAuthenticationFilter.class);
+	}
+	
+}
---
+++ src/main/java/com/g3/auth/config/security/TokenService.java
@@ -0,0 +1,52 @@
+package com.g3.auth.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import com.g3.auth.model.Admin;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+	
+	@Value("${auth.jwt.expiration}")
+	private String expiration;
+	
+	@Value("${auth.jwt.secret}")
+	private String secret;
+	
+	public String generateToken(Authentication authentication) {
+		Admin loggedAdmin = (Admin) authentication.getPrincipal();
+		Date today = new Date();
+		Date expirationDate = new Date(today.getTime() + Long.parseLong(expiration));
+		
+		return Jwts.builder()
+				.setIssuer("G3 - Auth API")
+				.setSubject(loggedAdmin.getId().toString())
+				.setIssuedAt(today)
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.setExpiration(expirationDate)
+				.compact();
+	}
+	
+	public boolean isTokenValid(String token) {
+		try {
+			Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public Long getUserId (String token) {
+		Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+		return Long.parseLong(claims.getSubject());
+	}
+	
+}
