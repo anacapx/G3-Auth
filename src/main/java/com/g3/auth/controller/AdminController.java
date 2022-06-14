ddc986d55bc310b580f4973818d8ccfbf8287701
lisdrl <lisandre.darioli@gmail.com>
2022-06-14 15:25:04 -0300

    feat(back): refactoring code in order to both add custom exceptions and implement service interface and impl class

--- src/main/java/com/g3/auth/controller/AdminController.java
+++ src/main/java/com/g3/auth/controller/AdminController.java
@@ -1,18 +1,11 @@
 package com.g3.auth.controller;
 
-import java.util.Optional;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,16 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.g3.auth.config.security.TokenDTO;
-import com.g3.auth.config.security.TokenService;
 import com.g3.auth.controller.dto.AdminDTO;
 import com.g3.auth.controller.form.LoginForm;
 import com.g3.auth.controller.form.SignUpForm;
-import com.g3.auth.model.Admin;
-import com.g3.auth.repository.AdminRepository;
-import com.g3.auth.repository.ProfileRepository;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import com.g3.auth.service.impl.AdminService;
 
 @RestController
 @RequestMapping("/")
@@ -39,78 +26,31 @@ import io.jsonwebtoken.Jwts;
 public class AdminController {
 
 	@Autowired
-	private AdminRepository adminRepository;
-
-	@Autowired
-	private ProfileRepository profileRepository;
-	
-	@Autowired
-	private AuthenticationManager authManager;
-
-	@Autowired
-	TokenService tokenService;
-	
-	@Value("${auth.jwt.secret}")
-	private String secret;
+	private AdminService admService;
 
 	@GetMapping("admin/{id}")
 	public ResponseEntity<AdminDTO> findAdmById(@PathVariable Long id) {
-		Optional<Admin> adminOptional = adminRepository.findById(id);
-		if (adminOptional.isPresent()) {
-			AdminDTO adminDTO = new AdminDTO(adminOptional.get());
-			return ResponseEntity.ok(adminDTO);
-		}
-		return ResponseEntity.notFound().build();
+		AdminDTO adminDTO = admService.findAdmById(id);
+		return ResponseEntity.ok(adminDTO);
 	}
 
 	@PostMapping("admin")
 	public ResponseEntity<AdminDTO> signUp(@RequestBody @Valid SignUpForm form) {
-
-		Optional<Admin> adminByEmail = adminRepository.findByEmail(form.getEmail());
-		if (adminByEmail.isPresent()) {
-			return ResponseEntity.badRequest().build();
-		}
-		Admin newAdmin = adminRepository.save(form.toAdmin(profileRepository));
-		AdminDTO adminDTO = new AdminDTO(newAdmin);
+		AdminDTO adminDTO = admService.signUp(form);
 		return ResponseEntity.ok(adminDTO);
 	}
-	
+
 	@PostMapping("login")
 	public ResponseEntity<TokenDTO> authenticate(@RequestBody @Valid LoginForm form) {
-		UsernamePasswordAuthenticationToken loginData = form.toUsernamePasswordAuthenticationToken();
-		try {
-			Authentication authentication = authManager.authenticate(loginData);
-			String token = tokenService.generateToken(authentication);
-			return ResponseEntity.ok(new TokenDTO(token, "Bearer"));
-		} catch (AuthenticationException e) {
-			return ResponseEntity.badRequest().build();
-		}	
+		TokenDTO tokenDTO = admService.authenticate(form);
+		return ResponseEntity.ok(tokenDTO);
 	}
-	
+
 	@GetMapping("validate")
 	public ResponseEntity<Authentication> validateToken(HttpServletRequest request) {
-		String token = request.getHeader("Authorization");
-		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
-			return ResponseEntity.badRequest().build();
-		}
-		token = token.substring(7, token.length());
-
-//		Gets user Id
-		Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
-		Long userId = Long.parseLong(claims.getSubject());
-
-//		Gets user
-		Admin user = adminRepository.findById(userId).get();
-
-		SecurityContextHolder.getContext()
-				.setAuthentication(new UsernamePasswordAuthenticationToken(user, token, user.getAuthorities()));
-		
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication == null) {
-			return ResponseEntity.badRequest().build();
-		}
+		Authentication authentication = admService.validateToken(request);
 		return ResponseEntity.ok(authentication);
 
 	}
-	
+
 }
